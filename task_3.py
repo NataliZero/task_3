acb6f5cdf883d8d0acedd6bb74360ebbccccc72d
NataliZero <ball.diamond.ru@gmail.com>
2024-10-21 11:50:07 +0300

    update code
    окончательный вариант

--- task_3.py
+++ task_3.py
@@ -1,11 +1,17 @@
 import requests
 
+# Данные для отправки
 data = {
     'title': 'foo',
     'body': 'bar',
     'userId': 1
 }
+
+# Отправляем POST-запрос
 response = requests.post('https://jsonplaceholder.typicode.com/posts', data=data)
 
+# Проверяем статус-код
 print(response.status_code)
+
+# Выводим ответ
 print(response.json())
